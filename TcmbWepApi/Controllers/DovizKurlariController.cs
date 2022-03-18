84b325e3380fd27e35a23b6f70f69f5fd0514d57
Ali Akpınar <94609325+AliAkpnr@users.noreply.github.com>
2022-03-18 12:33:34 +0300

    Add project files.

---
+++ TcmbWepApi/Controllers/DovizKurlariController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace TcmbWepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DovizKurlariController : ControllerBase
+    {
+        String url = "https://www.tcmb.gov.tr/kurlar/today.xml";
+
+        [HttpGet("today")]
+        public IActionResult Today()
+        {
+            var httpClient = new HttpClient();
+            var result = httpClient.GetAsync(url).Result;
+
+            var stream = result.Content.ReadAsStreamAsync().Result;
+
+            var itemXml = XElement.Load(stream); 
+            Tarih_Date tarih_Date;
+            XmlSerializer serializer = new XmlSerializer(typeof(Tarih_Date));
+            using (TextReader reader = new StringReader(itemXml.ToString()))
+            {
+                tarih_Date = (Tarih_Date)serializer.Deserialize(reader);
+            }
+
+
+
+            return Ok(tarih_Date);
+        }
+    }
+}
---
+++ TcmbWepApi/Tarih_Date.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace TcmbWepApi
+
+{
+    [XmlRoot(ElementName = "Tarih_Date")]
+	public class Tarih_Date
+	{
+		[XmlElement(ElementName = "Currency")]
+		public List<Currency> Currency { get; set; }
+		[XmlAttribute(AttributeName = "Tarih")]
+		public string Tarih { get; set; }
+		[XmlAttribute(AttributeName = "Date")]
+		public string Date { get; set; }
+		[XmlAttribute(AttributeName = "Bulten_No")]
+		public string Bulten_No { get; set; }
+	}
+
+}
+
